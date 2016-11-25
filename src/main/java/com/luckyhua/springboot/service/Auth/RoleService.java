24cee4fb9080ed4c5b477256ebd5ccaadca106cc
zhough <zhouguanghua@liegouchina.com>
2016-11-25 18:00:03 +0800

    增加security认证授权

---
+++ src/main/java/com/luckyhua/springboot/service/Auth/RoleService.java
@@ -0,0 +1,17 @@
+package com.luckyhua.springboot.service.auth;
+
+import com.luckyhua.springboot.model.Role;
+import java.util.List;
+
+/**
+ * @author luckyhua
+ * @date 2016/11/25
+ * @description
+ */
+public interface RoleService {
+
+    List<Role> getUserRoles(Integer userId);
+
+    Boolean authorized(Integer userId, String s, String b);
+
+}
